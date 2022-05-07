68cd15961122e7422f1c4a9e9bc830fab2b313a5
Matúš Jurko <matus.jurko.dev@outlook.com>
2022-05-07 13:17:20 +0200

    Tidy up Navbar and Footer component

--- src/components/ui/Footer/index.tsx
+++ src/components/ui/Footer/index.tsx
@@ -1,12 +1,38 @@
-import FooterMenu from './FooterMenu';
+import { footer } from '../../../config';
 import FooterLink from './FooterLink';
 import Container from '../Container';
 
 const Footer = () => {
   return (
-    <footer>
-      <FooterMenu />
-      <div className="bg-extra-light-gray py-5">
+    <footer className="border-t bg-white">
+      <Container className="grid gap-8 py-8 md:grid-cols-2 lg:grid-cols-3">
+        {footer.map(({ title, links }) => (
+          <div key={title}>
+            <h5 className="mb-2 whitespace-nowrap text-md font-semibold tracking-tight text-black">
+              {title}
+            </h5>
+            <ul>
+              {links.map(({ name, href, icon: Icon }) => (
+                <li key={name}>
+                  <FooterLink
+                    className="h-9 w-full sm:w-auto"
+                    rel="noreferrer nofollow"
+                    target="_blank"
+                    href={href}
+                  >
+                    <Icon
+                      className="mr-2 text-lg text-medium-gray"
+                      aria-hidden
+                    />
+                    {name}
+                  </FooterLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Container>
+      <div className="border-t bg-extra-light-gray py-5">
         <Container className="text-center text-sm text-black sm:text-left">
           &copy; Copyright{' '}
           <FooterLink href="/" className="font-semibold">
--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -1,8 +1,10 @@
 import { Disclosure } from '@headlessui/react';
+import { navigation } from '../../../config';
 import NavbarToggle from './NavbarToggle';
-import NavbarLogo from './NavbarLogo';
-import NavbarMenu from './NavbarMenu';
+import NavbarLink from './NavbarLink';
 import Container from '../Container';
+import Button from '../Button';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
@@ -11,14 +13,81 @@ const Navbar = () => {
         <>
           <Container className="flex h-14 items-center space-x-2 xl:space-x-3">
             <div className="relative flex-grow lg:flex-grow-0">
-              <NavbarLogo />
+              <div className="flex items-center">
+                <NavbarLink
+                  href="https://www.microsoft.com"
+                  rel="external nofollow"
+                  target="_blank"
+                >
+                  <Image
+                    src="/images/microsoft-logo.png"
+                    alt="Microsoft logo"
+                    width={108}
+                    height={23}
+                  />
+                </NavbarLink>
+                <div className="mx-2 h-6 w-[2px] bg-extra-dark-gray md:mx-4" />
+                <NavbarLink
+                  className="text-md font-semibold tracking-tight text-medium-gray lg:hover:underline lg:hover:decoration-medium-gray lg:hover:decoration-2 lg:hover:underline-offset-[5px]"
+                  href="/"
+                >
+                  <span className="sm:hidden">ŠTC</span>
+                  <span className="hidden sm:inline">
+                    Študentské Trénerské Centrum
+                  </span>
+                </NavbarLink>
+              </div>
               <div className="absolute inset-y-0 right-0 lg:hidden">
                 <NavbarToggle open={open} />
               </div>
             </div>
-            <NavbarMenu />
+            <div className="hidden flex-grow items-center justify-between lg:flex">
+              <ul className="flex flex-shrink-0 space-x-2 xl:space-x-3">
+                {navigation.map(({ name, href }) => (
+                  <li key={name}>
+                    <NavbarLink
+                      className="text-sm text-black hover:underline hover:decoration-extra-dark-gray hover:decoration-2 hover:underline-offset-[5px]"
+                      href={href}
+                    >
+                      {name}
+                    </NavbarLink>
+                  </li>
+                ))}
+              </ul>
+              <Button
+                title="Registrácia do programu ŠTC"
+                href="/registracia"
+                type="default"
+              >
+                Registrácia
+              </Button>
+            </div>
           </Container>
-          {open && <NavbarMenu.Mobile />}
+          <Disclosure.Panel className="border-t bg-extra-light-gray">
+            <Container as="ul">
+              {navigation.map(({ name, href }) => (
+                <Disclosure.Button
+                  className="border-t py-1 first:border-0"
+                  key={name}
+                  as="li"
+                >
+                  <NavbarLink href={href} className="w-full text-black">
+                    {name}
+                  </NavbarLink>
+                </Disclosure.Button>
+              ))}
+              <div className="pt-1 pb-4">
+                <Button
+                  title="Registrácia do programu ŠTC"
+                  href="/registracia"
+                  type="outline"
+                  fullWidth
+                >
+                  Registrácia
+                </Button>
+              </div>
+            </Container>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
