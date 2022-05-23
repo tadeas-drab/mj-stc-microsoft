20ea2281c68cb11f8aaf3e7689d9264ca42a1ef5
Matúš Jurko <matus.jurko.dev@outlook.com>
2022-05-23 14:04:13 +0200

    Add correct favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,33 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className="scroll-smooth" lang="sk">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg"
+            color="#107c10"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
